002b4716b95c904c138039d68d90e4af1ec0f30c
Alex Shchapov <alexshchapov02@gmail.com>
2023-12-15 11:34:06 +0200

    created transportchan directory

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/Matterlinkk/Dech-Node/block"
 	"github.com/Matterlinkk/Dech-Node/transaction"
+	"github.com/Matterlinkk/Dech-Node/transportchan"
 	"github.com/Matterlinkk/Dech-Node/user"
 	"github.com/Matterlinkk/Dech-Wallet/signature"
 	"math/big"
@@ -30,73 +31,29 @@ func main() {
 	channelBlock := make(chan block.Block)
 
 	go func() {
-		ProcessBlockchain(channelBlock, db)
+		transportchan.ProcessBlockchain(channelBlock, db)
 	}()
 
 	go func() {
-		ProcessBlock(channelBlock, channelTnx, db)
+		transportchan.ProcessBlock(channelBlock, channelTnx, db)
 	}()
 
 	go func() {
 		time.Sleep(6 * time.Second)
-		TnxToBlock(channelTnx, *tnx2)
+		transportchan.TnxToBlock(channelTnx, *tnx2)
 	}()
 
 	go func() {
 		time.Sleep(21 * time.Second)
-		TnxToBlock(channelTnx, *tnx3)
+		transportchan.TnxToBlock(channelTnx, *tnx3)
 	}()
 
 	go func() {
-		TnxToBlock(channelTnx, *tnx1)
+		transportchan.TnxToBlock(channelTnx, *tnx1)
 	}()
 
 	time.Sleep(30 * time.Second)
 	fmt.Println("Db: \n")
 	db.KeyPair()
 
-}
-
-func BlockToBlockchain(SetOfBlock chan block.Block, block block.Block) {
-	SetOfBlock <- block
-}
-
-func TnxToBlock(setOfTnx chan transaction.Transaction, tnx transaction.Transaction) {
-	setOfTnx <- tnx
-}
-
-func ProcessBlock(chanBlock chan block.Block, chanTnx chan transaction.Transaction, db *block.Blockchain) {
-	for {
-		set := block.CreateSetOfTransactions()
-
-		var timer *time.Timer
-
-	CollectTransactions:
-		for {
-			if timer == nil {
-				timer = time.NewTimer(5 * time.Second)
-			} else {
-				timer.Reset(5 * time.Second)
-			}
-
-			select {
-			case tnx := <-chanTnx:
-				set = append(set, tnx)
-			case <-timer.C:
-				fmt.Println("Creating block")
-				newBlock := block.CreateBlock(set, db)
-				fmt.Println("Block: ", newBlock)
-				BlockToBlockchain(chanBlock, *newBlock)
-				break CollectTransactions
-			}
-		}
-	}
-}
-
-func ProcessBlockchain(setOfBlock chan block.Block, blockchain *block.Blockchain) {
-	for b := range setOfBlock {
-		blockchain.Mutex.Lock()
-		blockchain.AddBlock(b)
-		blockchain.Unlock()
-	}
 }
---
+++ transportchan/transport.go
@@ -0,0 +1,52 @@
+package transportchan
+
+import (
+	"fmt"
+	"github.com/Matterlinkk/Dech-Node/block"
+	"github.com/Matterlinkk/Dech-Node/transaction"
+	"time"
+)
+
+func BlockToBlockchain(SetOfBlock chan block.Block, block block.Block) {
+	SetOfBlock <- block
+}
+
+func TnxToBlock(setOfTnx chan transaction.Transaction, tnx transaction.Transaction) {
+	setOfTnx <- tnx
+}
+
+func ProcessBlock(chanBlock chan block.Block, chanTnx chan transaction.Transaction, db *block.Blockchain) {
+	for {
+		set := block.CreateSetOfTransactions()
+
+		var timer *time.Timer
+
+	CollectTransactions:
+		for {
+			if timer == nil {
+				timer = time.NewTimer(5 * time.Second)
+			} else {
+				timer.Reset(5 * time.Second)
+			}
+
+			select {
+			case tnx := <-chanTnx:
+				set = append(set, tnx)
+			case <-timer.C:
+				fmt.Println("Creating block")
+				newBlock := block.CreateBlock(set, db)
+				fmt.Println("Block: ", newBlock)
+				BlockToBlockchain(chanBlock, *newBlock)
+				break CollectTransactions
+			}
+		}
+	}
+}
+
+func ProcessBlockchain(setOfBlock chan block.Block, blockchain *block.Blockchain) {
+	for b := range setOfBlock {
+		blockchain.Mutex.Lock()
+		blockchain.AddBlock(b)
+		blockchain.Unlock()
+	}
+}
